7451de0b7ad52498f57acadb8a6b42b31b7db327
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2024-08-21 11:24:31 +0000

    resolved conflicts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
-// src/app/layout.js
 import ClientLayout from "./clientlaysout.js";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import Script from 'next/script';
 
 export const metadata = {
   title: "Sustained Paths",
@@ -10,12 +10,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-MCZXSESDDH`}
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MCZXSESDDH');
+          `}
+        </Script>
+      </head>
       <body>
-        
-      <UserProvider>
-        <ClientLayout>
-          {children}
-        </ClientLayout>
+        <UserProvider>
+          <ClientLayout>
+            {children}
+          </ClientLayout>
         </UserProvider>
       </body>
     </html>
